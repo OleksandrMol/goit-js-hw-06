840e1a6b55c22e296f1ca18a56b28154bc7bf96a
OleksandrMol <raydar@ukr.net>
2023-04-20 12:43:17 +0300

    8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', subFunction);
+
+function subFunction (evt) {
+    evt.preventDefault();
+    const { elements: { email, password } } = evt.currentTarget;
+    const data = {
+        Email: [email.value],
+        Password: [password.value],
+    }
+    if (email.value === '' || password.value === '') {
+        return window.alert('Заповніть всі поля')
+    }
+    
+    console.log(data)
+    evt.currentTarget.reset();
+         
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const span = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", getColor);
+
+function getColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  span.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
