7e6a55f8c01810f909ded2a09cf370eb847e6afa
OleksandrMol <raydar@ukr.net>
2023-04-24 15:52:45 +0300

    remmast

--- js/task-02.js
+++ js/task-02.js
@@ -4,12 +4,16 @@ const ingredients = [
   'Garlic',
   'Tomatos',
   'Herbs',
-  'Condiments',
-  
+  'Condiments',  
 ];
  
 const ulList = document.getElementById('ingredients');
 
-const ing = ingredients.map((ingredient)=>`<li>${ingredient}</li>`).join('')
+const ing = ingredients.map((ingredient) => {
+ const li = document.createElement('li');
+  li.classList.add("item");
+  li.textContent = ingredient;
+  return li
+})
 
-ulList.insertAdjacentHTML('afterbegin', ing)
+ulList.append(...ing);
